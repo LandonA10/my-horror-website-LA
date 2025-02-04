8071fecfadd5390dcdbf375b54d74644a5703aa0
LandonA <PVZBOYGIRL@gmail.com>
2025-02-03 20:23:46 -0800

    final adon image added

--- src/app/characters/page.js
+++ src/app/characters/page.js
@@ -43,6 +43,10 @@ export default function Characters(){
                             <p className="regionBackParagraph">To be realistic, if you encounter this Horror, your chance of survival is less than 1%. However, if you were truly determined to stay alive, here is the guide to try and survive. First off, the Adon likes to stay in places with little light, as the dark is where fear is most prominent. When traveling through the forests of Mourn, try to avoid extremely dark areas where even the sun ceases to reach. If by unlucky fate, you were to encouter this Horror, keep calm and try to relax yourself. The more scared you are, the stronger the Adon gets. It is physically and mentally impossible to not be atleast a little scared of the Adon, so it is crucial to reduce your fear as much as possible. Slowly, back up until you reach an area of light. Once you feel the sun or some sort of light on your back, run. Run. Run, and don't look back, maybe then it would spare your life.</p>
                             </div>
                         </div>
+                        <div className="charImagesDiv">
+                            <ImageFigure name={"The Adon"} src={""} />
+                            <ImageFigure name={"The Adon"} src={""} />
+                        </div>
                     </div>
                 </section>
             </section>
@@ -57,6 +61,18 @@ function RegionLink({regionName, regionLink}){
     return (
         <>
             <a className="regionLink" href = {regionLink}>{regionName}</a>
+        </>
+    )
+}
+
+function ImageFigure({name, src}){
+
+    return (
+        <>
+            <figure className="charImageFig">
+                <img className="charImage" src={src} alt={name} />
+                <figcaption>{name}</figcaption>
+            </figure>
         </>
     )
 }
